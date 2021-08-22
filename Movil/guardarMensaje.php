252ecddc34a337a47e371443c0f91c31c3bf5df6
AldaCm <89363779+AldaCm@users.noreply.github.com>
2021-08-22 18:57:21 -0500

    primera versión

---
+++ Movil/guardarMensaje.php
@@ -0,0 +1,15 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+
+    $correo = $_REQUEST["correoM"];
+    $mensaje = $_REQUEST["mensajeM"];
+
+    require("conexion.php");
+
+    $sql = "INSERT INTO tblMensajes VALUES('$correo','$mensaje',NOW())";
+    
+    mysqli_query($conexion, $sql);
+    mysqli_close($conexion);
+    
+    echo "Registrado";
+?>
