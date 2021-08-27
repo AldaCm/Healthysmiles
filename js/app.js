1cbfe6f8767ab6764122fd5fe1039b4effd91903
AldaCm <89363779+AldaCm@users.noreply.github.com>
2021-08-26 21:30:39 -0500

    modificacion 3

--- js/app.js
+++ js/app.js
@@ -327,7 +327,7 @@ function cerrarSesion(){
 
 if ('serviceWorker' in navigator){
     window.addEventListener('load', () =>{
-        navigator.serviceWorker.register('..//sw.js'.then)(() =>{
+        navigator.serviceWorker.register('../sw.js'.then)(() =>{
             console.log('Service Worker Registered')
         });
     });
